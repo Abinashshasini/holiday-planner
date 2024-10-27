65278bc518f2458ce6ac5ed36f0ff024ea466e96
newusername <=>
2024-10-27 16:19:38 +0530

    RFQ card creation commit

---
+++ src/components/rfq-card/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './rfqCard.module.scss';
+
+const RFQCard = () => {
+  return <div className={classes.container}>RFQCard</div>;
+};
+
+export default RFQCard;
