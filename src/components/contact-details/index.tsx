69719153a62ebcc2be4e1de0cf6770f510fc31a5
newusername <=>
2024-11-29 22:23:44 +0530

    Layout updated

--- src/components/contact-details/index.tsx
+++ src/components/contact-details/index.tsx
@@ -2,11 +2,26 @@
 import React from 'react';
 import Heading from '../heading';
 import { MdPhoneCallback } from 'react-icons/md';
+import { IoIosMail } from 'react-icons/io';
 import classes from './contact-details.module.scss';
 
 const ContactDetails = () => {
+  /** Function to call owner */
   const handleCallVendor = (number: string) => {
     window.location.href = `tel:${number}`;
+  };
+
+  /** Function to mail owner */
+  const handleMailOwner = () => {
+    const email = 'holidayplannertravel@gmail.com';
+    const subject = encodeURIComponent('Inquiry About Holiday planner');
+    const body = encodeURIComponent(
+      "Hello,\n\nI got to know about Holiday Planner, I'm interested to know more about services.\n\nThank you!"
+    );
+
+    const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
+
+    window.location.href = mailtoLink;
   };
 
   return (
@@ -22,7 +37,12 @@ const ContactDetails = () => {
           <p onClick={() => handleCallVendor('8249262728')}>+918249262728</p>
           <p onClick={() => handleCallVendor('7008345662')}>+917008345662</p>
         </div>
+        <div className={classes.contactNumberCnt} onClick={handleMailOwner}>
+          <IoIosMail />
+          <p>holidayplannertravel@gmail.com</p>
+        </div>
       </address>
+
       <h3 className={classes.copyRight}>
         © {new Date().getFullYear()} All Rights Reserved. Designed and Developed
         by <a href="https://abinashshasini.vercel.app/">Abinash Shasini</a>
